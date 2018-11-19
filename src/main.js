568b928a6c58367d1c1fdbdc3060defc047498f0
wpx <wangpx@minstone.com.cn>
2018-11-19 10:59:37 +0800

    注入设置axios请求

--- src/main.js
+++ src/main.js
@@ -4,10 +4,13 @@ import router from './router/index'
 import store from './vuex/store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+// 引入axios
+import axios from 'axios';
 
 // 注入Element
 Vue.use(ElementUI);
-
+// 注入axios
+Vue.prototype.$axios = axios;
 Vue.config.productionTip = false
 
 new Vue({
